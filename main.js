bc48aa55c468d0988011f40dd115213bf2a6cdf8
misanorot <audi16v@gmx.de>
2020-02-01 11:49:07 +0100

    fixed little issue

--- main.js
+++ main.js
@@ -736,9 +736,8 @@ function main() {
 }
 //#############HELPERS#######################################################################################
 function force(id){
-    const force_step = 'get' + id;
     try{
-        toPoll[force_step].lastPoll = 0;
+        toPoll[id].lastPoll = 0;
     }catch(e){
         adapter.log.warn(`Force polling interval: ${id} not incude in get states`);
     }
